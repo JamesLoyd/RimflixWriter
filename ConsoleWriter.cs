ad901c52f15b37f1ed3eafb505ae0c29622385b6
JamesLoyd <jamestloyd@gmail.com>
2025-07-21 09:51:13 -0400

    Add pastel and lay ground work for better logging

---
+++ ConsoleWriter.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+using Pastel;
+
+namespace RimflixShowMaker;
+
+public static class ConsoleWriter
+{
+    //TODO: This will be replaced with a proper logging framework in the future.
+    // I just copied it over from existing code cause, it was simple and effective.
+    public static void WriteInformation(string message)
+    {
+        var infoLabel = "SYSTEM".Pastel(Color.Chartreuse);
+        Console.WriteLine($"[{infoLabel}] {message}");
+    }
+}
